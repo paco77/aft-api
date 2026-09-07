51b3e0dc108088a55804b017529766934e3efeac
paco77 <fpuentecortez@gmail.com>
2026-09-07 17:38:46 -0600

    fix: corregir import de Model en OutdoorActivity

--- app/Models/OutdoorActivity.php
+++ app/Models/OutdoorActivity.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\tModel;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class OutdoorActivity extends Model
